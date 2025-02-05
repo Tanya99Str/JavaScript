abbf91948d6d0237c6d3a82a225d14586d0fad1c
Tanya99Str <tetjana00iris@gmail.com>
2025-02-05 00:09:29 -0400

    first
    lesson 1

--- lesson 1/main.js
+++ lesson 1/main.js
@@ -52,8 +52,6 @@
 // let greeting = `Hello, I'm ${lastName} ${firstName} ${middleName}!`;
 // console.log(greeting);
 
-
-
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 //     let a = 100; let b = '100'; let c = true;
 let a = 100;
